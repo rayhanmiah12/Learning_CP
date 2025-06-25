79c5b63b47eb59c82297027dd8b27b46b138ef4c
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-06-26 00:34:40 +0600

    Sheet 1-B_Basic_Data_Types

---
+++ Assuit ICPC/Sheet #1/B_Basic_Data_Types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a;
+    long long b;
+    char c;
+    float d;
+    double e;
+
+    // Reading input
+    cin >> a >> b >> c >> d >> e;
+
+    // Printing each value on a new line
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << d << endl;
+    cout << e << endl;
+
+    return 0;
+}
