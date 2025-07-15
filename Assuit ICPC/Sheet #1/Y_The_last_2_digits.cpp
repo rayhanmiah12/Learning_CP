219763d490e3051a72ce373e809cdeb5148b942b
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-07-15 20:07:16 +0600

    Sheet 1-Y_The_last_2_digits

---
+++ Assuit ICPC/Sheet #1/Y_The_last_2_digits.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <iomanip> 
+using namespace std;
+
+int main() {
+    long long a, b, c, d;
+    cin >> a >> b >> c >> d;
+    
+    long long result = 1;
+    result = (result * (a % 100)) % 100;
+    result = (result * (b % 100)) % 100;
+    result = (result * (c % 100)) % 100;
+    result = (result * (d % 100)) % 100;
+
+    cout << setw(2) << setfill('0') << result << endl;
+
+    return 0;
+}
