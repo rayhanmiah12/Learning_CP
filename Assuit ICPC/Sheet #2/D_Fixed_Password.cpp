3fb33a2a5cf130f426beeebdeb6739e933326ab2
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-07-19 10:04:00 +0600

    Sheet 2-\D_Fixed_Password

---
+++ Assuit ICPC/Sheet #2/D_Fixed_Password.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int X;
+    for(;;){ cin>>X;
+    if(X==1999)
+    {
+        cout<<"Correct"<<'\n';
+        break;
+    }
+    else{
+        cout<<"Wrong"<<'\n';
+    }
+}
+
+   return 0;
+}
