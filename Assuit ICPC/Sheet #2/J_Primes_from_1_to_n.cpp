6e5e4b782f2e9f616e2ca358d508064c3062bebb
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-08-01 07:28:13 +0600

    Sheet 2-J_Primes_from_1_to_n

---
+++ Assuit ICPC/Sheet #2/J_Primes_from_1_to_n.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N; cin >> N;
+    for(int i=2;i<=N;i++)
+    { 
+        int f = 0;
+        for(int j=2;j<i;j++)
+        {
+           if(i%j==0)
+           {
+            f = 1;
+            break;
+           }
+        }
+        if(f==0)
+        {
+            cout<<i<<" ";
+        }
+    }
+    cout << '\n';
+}
