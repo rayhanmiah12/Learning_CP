29e6c3bda5cc4b3c152846d8a64627cd1cf9f1af
rayhanmiah12 <rayhanmiah5562@gmail.com>
2025-08-21 09:04:43 +0600

    Watermelon

---
+++ Assuit ICPC/Sheet #2/Watermelon.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int w;
+    if(cin >> w)
+    {
+     if(w>2 && w%2 == 0) 
+     {
+       cout << "YES\n";
+     }
+     else 
+     {
+        cout << "NO\n";
+     }
+
+    }
+    return 0;
+
+}
